94d86ace5274a5368cc36a0cc44d2ff1dc916ab3
Aliaksei <alexx333by@gmail.com>
2018-11-21 23:11:08 +0300

    DAO interface implemented

---
+++ src/by/bsuir/kaziukovich/archive/server/dataaccess/DaoException.java
@@ -0,0 +1,7 @@
+package by.bsuir.kaziukovich.archive.server.dataaccess;
+
+public class DaoException extends Exception {
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
