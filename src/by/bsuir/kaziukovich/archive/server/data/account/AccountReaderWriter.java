9a376e1aeba5d363ba5caf1480b57fcf8ee4c022
Aliaksei <alexx333by@gmail.com>
2018-11-21 22:31:32 +0300

    AccountReaderWriter implemented

---
+++ src/by/bsuir/kaziukovich/archive/server/data/account/AccountReaderWriter.java
@@ -0,0 +1,6 @@
+package by.bsuir.kaziukovich.archive.server.data.account;
+
+import by.bsuir.kaziukovich.archive.domain.Account;
+import by.bsuir.kaziukovich.archive.server.data.ListReaderWriter;
+
+public interface AccountReaderWriter extends ListReaderWriter<Account> { }
--- src/by/bsuir/kaziukovich/archive/server/data/account/impl/AccountXmlFileReaderWriter.java
+++ src/by/bsuir/kaziukovich/archive/server/data/account/impl/AccountXmlFileReaderWriter.java
@@ -1,8 +1,8 @@
-package by.bsuir.kaziukovich.archive.server.data.record.impl;
+package by.bsuir.kaziukovich.archive.server.data.account.impl;
 
-import by.bsuir.kaziukovich.archive.domain.StudentRecord;
+import by.bsuir.kaziukovich.archive.domain.Account;
 import by.bsuir.kaziukovich.archive.server.data.ReadWriteException;
-import by.bsuir.kaziukovich.archive.server.data.record.StudentRecordReaderWriter;
+import by.bsuir.kaziukovich.archive.server.data.account.AccountReaderWriter;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.FileInputStream;
@@ -11,36 +11,36 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentRecordXmlFileReaderWriter implements StudentRecordReaderWriter {
+public class AccountXmlFileReaderWriter implements AccountReaderWriter {
     @Override
-    public List<StudentRecord> readFrom(String path) throws ReadWriteException {
+    public List<Account> readFrom(String path) throws ReadWriteException {
         if (path == null) {
             throw new IllegalArgumentException("Path shouldn't be null");
         }
 
         try (XMLDecoder decoder = new XMLDecoder(new FileInputStream(path))) {
-            return (List<StudentRecord>) decoder.readObject();
+            return (List<Account>) decoder.readObject();
         } catch (FileNotFoundException e) {
             throw new ReadWriteException("File with path " + path + " was not found", e);
         } catch (ClassCastException e) {
-            throw new ReadWriteException("File with path " + path + " doesn't contain student records", e);
+            throw new ReadWriteException("File with path " + path + " doesn't contain accounts", e);
         }
     }
 
     @Override
-    public void writeTo(List<StudentRecord> records, String path) throws ReadWriteException {
-        if ((records == null) || (path == null)) {
+    public void writeTo(List<Account> accounts, String path) throws ReadWriteException {
+        if ((accounts == null) || (path == null)) {
             throw new IllegalArgumentException("Arguments shouldn't be null");
         }
 
         try (XMLEncoder encoder = new XMLEncoder(new FileOutputStream(path))) {
-            List<SerializableStudentRecord> toWrite = new ArrayList<>();
+            List<SerializableAccount> toWrite = new ArrayList<>();
             ReadWriteException aggregatedIllegalArgumentExceptions
-                    = new ReadWriteException("Cannot cast some records");
+                    = new ReadWriteException("Cannot cast some accounts");
 
-            for (StudentRecord record : records) {
+            for (Account account : accounts) {
                 try {
-                    toWrite.add(new SerializableStudentRecord(record));
+                    toWrite.add(new SerializableAccount(account));
                 } catch (IllegalArgumentException e) {
                     aggregatedIllegalArgumentExceptions.addSuppressed(e);
                 }
