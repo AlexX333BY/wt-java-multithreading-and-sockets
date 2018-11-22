6108193b8b5ba106de4d2d011f629f99b7a47dc4
Aliaksei <alexx333by@gmail.com>
2018-11-22 22:31:25 +0300

    AddAccount command implemented

---
+++ src/by/bsuir/kaziukovich/archive/server/logic/command/impl/account/AddAccountCommand.java
@@ -0,0 +1,30 @@
+package by.bsuir.kaziukovich.archive.server.logic.command.impl.account;
+
+import by.bsuir.kaziukovich.archive.domain.account.UserRole;
+import by.bsuir.kaziukovich.archive.server.dataaccess.DaoException;
+import by.bsuir.kaziukovich.archive.server.dataaccess.account.AccountDaoFactory;
+import by.bsuir.kaziukovich.archive.server.logic.command.Command;
+import by.bsuir.kaziukovich.archive.server.logic.command.CommandException;
+
+public class AddAccountCommand implements Command {
+    private static final byte REQUIRED_ARGUMENTS_COUNT = 2;
+
+    @Override
+    public String[] execute(String[] request) throws CommandException {
+        if (request.length < REQUIRED_ARGUMENTS_COUNT) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+
+        try {
+            AccountDaoFactory.getDao().add(request[0], request[1], UserRole.USER);
+            return null;
+        } catch (DaoException e) {
+            throw new CommandException("Error adding new account", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName();
+    }
+}
