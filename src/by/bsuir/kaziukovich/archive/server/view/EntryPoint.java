2e63c8625a7e12c59fead1e49740dbb9123c73eb
Aliaksei <alexx333by@gmail.com>
2018-11-23 00:11:02 +0300

    Server view: base added

---
+++ src/by/bsuir/kaziukovich/archive/server/view/EntryPoint.java
@@ -0,0 +1,46 @@
+package by.bsuir.kaziukovich.archive.server.view;
+
+import by.bsuir.kaziukovich.archive.domain.logger.Logger;
+import by.bsuir.kaziukovich.archive.server.dataaccess.DaoException;
+import by.bsuir.kaziukovich.archive.server.dataaccess.account.AccountDaoFactory;
+import by.bsuir.kaziukovich.archive.server.dataaccess.record.StudentRecordDaoFactory;
+import by.bsuir.kaziukovich.archive.server.view.impl.MultiThreadSocketListener;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        SocketListener listener;
+
+        if (args.length < 3) {
+            Logger.log("At least 3 arguments needed: port, account file path and records file path");
+            return;
+        }
+
+        if (args.length > 3) {
+            try {
+                Logger.setErrorStream(new PrintStream(args[3]));
+            } catch (FileNotFoundException e) {
+                Logger.log("Error setting error stream");
+                return;
+            }
+        }
+
+        try {
+            AccountDaoFactory.getDao().loadFrom(args[0]);
+            StudentRecordDaoFactory.getDao().loadFrom(args[1]);
+        } catch (DaoException e) {
+            Logger.log(e);
+            return;
+        }
+
+        try {
+            listener = new MultiThreadSocketListener(Integer.parseInt(args[0]));
+        } catch (IllegalArgumentException e) {
+            Logger.log(e);
+            return;
+        }
+        listener.startListen();
+    }
+}
---
+++ src/by/bsuir/kaziukovich/archive/server/view/SocketListener.java
@@ -0,0 +1,5 @@
+package by.bsuir.kaziukovich.archive.server.view;
+
+public interface SocketListener {
+    void startListen();
+}
